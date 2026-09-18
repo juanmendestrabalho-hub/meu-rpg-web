a8f76eaa7c9734555e528c0c1363538785b58336
Juan MB <juanmendestrabalho@gmail.com>
2026-09-17 21:32:31 -0300

    Clean up comments in main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
-
 import { GameEngine } from './game.js';
-// Em breve importaremos Player, UI, Quests aqui
 
-// Aguarda o DOM carregar totalmente para evitar referências nulas ou erros de renderização
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Inicializando RPG Medieval Web...");
-
-    // Instancia o motor principal do jogo
     const game = new GameEngine();
-
-    // Inicia o loop de renderização (Game Loop)
     game.start();
 });
